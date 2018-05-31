eef38f4bb65eb682314ee261990e5f324a86e6b2
Bideo Wego <bideowego@gmail.com>
2018-05-31 01:02:26 -0400

    Tweak

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const knex = require('knex')({ client: 'pg' });
 
-const query = knex.select('*').from('users').where('id', 1).toSQL().toNative();
+const query = knex
+  .select('*')
+  .from('users')
+  .where('id', 1)
+  .toSQL()
+  .toNative();
 
 console.log(query);
